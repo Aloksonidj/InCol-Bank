8c8b9d40fa69cc9581862d7de1371928c29ab9b5
Aloksonidj <aloksoni1947@gmail.com>
2024-09-07 17:10:24 +0530

    Redirection in NewUser

--- login/views.py
+++ login/views.py
@@ -3,26 +3,23 @@ from .models import Account as Acc
 
 # Create your views here.
 def Login(request):
-    data = {"name":None,"link":None,"url":None,"pin":None}
-
-    count = 0
     
-    if count > 0:
+    data = {"name":None,"link":None,"url":"","pin":None}
 
-        if request.method == "POST":
+    if request.method == "POST":
 
-            Account_no = int(request.POST["Account_no"])
-            Passwrd = request.POST["password"]
+        Account_no = int(request.POST["Account_no"])
+        Passwrd = request.POST["password"]
 
 
-            Account=Acc.objects.get(id=Account_no)
+        Account=Acc.objects.get(id=Account_no)
 
-            if Account.id == Account_no and Passwrd == Account.passwords:
+        if Account.id == Account_no and Passwrd == Account.passwords:
                 
-                data["pass"] = "Login Successfull....."
-                data["name"] = Account.user_name
+            data["pass"] = "Login Successfull....."
+            data["name"] = Account.user_name
 
-                return render(request,"login/Login.html",data)
+            return render(request,"login/Login.html",data)
 
     return render(request,"login/Login.html")
 
